b0923fa8783f6fbb9bfcaaa5c93a6dc27cb32e7d
Tiri Tuatai <tiri.jnr@gmail.com>
2024-02-21 20:42:10 +0800

    FC Functions Done

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function receivesAFunction (spy) {
+    console.log (spy())
+}
+
+function returnsANamedFunction () {
+    return function namedFunction() {}
+};
+
+function returnsAnAnonymousFunction () {
+    return function () {}
+}
